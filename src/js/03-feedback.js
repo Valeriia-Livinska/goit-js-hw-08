01cd05b4b4f62b4b2e2e95dd13c52d0bee2534a5
Valeriia-Livinska <valerylivi@gmail.com>
2022-09-06 18:50:06 +0300

    third task is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+var throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+form.addEventListener('input', onFormInput);
+// form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let inputObject = {};
+
+chekingPreviousEntrance();
+
+function onFormInput(event) {
+  const formData = new FormData(event.currentTarget);
+
+  for (let [key, value] of formData) {
+    inputObject[key] = value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputObject));
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log(inputObject);
+  form.reset();
+  localStorage.clear();
+}
+
+function chekingPreviousEntrance() {
+  const savedPreviousValues = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedPreviousValues = JSON.parse(savedPreviousValues);
+
+  if (parsedPreviousValues) {
+    email.value = parsedPreviousValues.email;
+    message.value = parsedPreviousValues.message;
+    inputObject = parsedPreviousValues;
+    
+    console.log('Previous input information was found');
+  }
+}
