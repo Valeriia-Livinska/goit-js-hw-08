963585333c89fede9d4854b39d96f3c29d278365
Valeriia-Livinska <valerylivi@gmail.com>
2022-09-07 23:28:35 +0300

    Update third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
@@ -6,21 +6,25 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('[name="email"]');
 const message = document.querySelector('[name="message"]');
 
-form.addEventListener('input', onFormInput);
-// form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 let inputObject = {};
 
 chekingPreviousEntrance();
 
-function onFormInput(event) {
-  const formData = new FormData(event.currentTarget);
+// function onFormInput(event) {
+//   const formData = new FormData(event.currentTarget);
 
-  for (let [key, value] of formData) {
-    inputObject[key] = value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputObject));
-  }
+//   for (let [key, value] of formData) {
+//     inputObject[key] = value;
+//     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputObject));
+//   }
+// }
+
+function onFormInput(event) {
+  inputObject = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputObject));
 }
 
 function onFormSubmit(event) {
@@ -39,7 +43,7 @@ function chekingPreviousEntrance() {
     email.value = parsedPreviousValues.email;
     message.value = parsedPreviousValues.message;
     inputObject = parsedPreviousValues;
-    
+
     console.log('Previous input information was found');
   }
 }
